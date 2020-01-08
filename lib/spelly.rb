5ad706ae9270d0377b58ce654175eedad4d545c1
Smith Júnior <smith.junior@icloud.com>
2020-01-08 11:01:34 -0300

    Added pt_BR dictionaries

--- lib/spelly.rb
+++ lib/spelly.rb
@@ -1,18 +1,21 @@
+# frozen_string_literal: true
+
 require 'hunspell-ffi'
+
 class Spelly
   attr_accessor :dict
 
-  def initialize language
-    path = File.expand_path("../lib/dict", File.dirname(__FILE__))
+  def initialize(language)
+    path = File.expand_path('../lib/dict', File.dirname(__FILE__))
     # path = "#{Gem.loaded_specs['spelly'].full_gem_path}/lib/dict"
     @dict = Hunspell.new(path, language)
   end
 
-  def spell_check words
+  def spell_check(words)
     results = []
     words.each do |word|
       unless @dict.check?(word)
-        results << {word: word, suggest: @dict.suggest(word)}
+        results << { word: word, suggest: @dict.suggest(word) }
       end
     end
     results
